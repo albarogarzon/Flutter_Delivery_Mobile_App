d784134c83c33522197877c93d599295bfa31334
Dario <darioemanuelmarquez@gmail.com>
2020-08-29 13:08:09 -0300

    Project Created

---
+++ isw_implementacion_us_08_g5/lib/screens/pedir_lo_que_sea/main_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PedirLoQueSeaMainScreen extends StatefulWidget {
+  PedirLoQueSeaMainScreen();
+
+  @override
+  _PedirLoQueSeaMainScreenState createState() =>
+      _PedirLoQueSeaMainScreenState();
+}
+
+class _PedirLoQueSeaMainScreenState extends State<PedirLoQueSeaMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
