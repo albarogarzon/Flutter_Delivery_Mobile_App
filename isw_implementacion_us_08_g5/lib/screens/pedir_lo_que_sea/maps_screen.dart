9b737b7616666a49414ae50dfa8ae9955e5a485b
albarogarzon <53953421+albarogarzon@users.noreply.github.com>
2020-09-02 20:58:03 -0300

    Se guarda la direccion (en froma human-readable) en la variable selectedPlace.formattedAddress

--- isw_implementacion_us_08_g5/lib/screens/pedir_lo_que_sea/maps_screen.dart
+++ isw_implementacion_us_08_g5/lib/screens/pedir_lo_que_sea/maps_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:isw_implementacion_us_08_g5/models/Direccion.dart';
 import 'package:isw_implementacion_us_08_g5/providers/DireccionRetiroProvider.dart';
@@ -10,8 +9,7 @@ import 'package:google_maps_place_picker/google_maps_place_picker.dart';
 class MapsScreen extends StatefulWidget {
   MapsScreen();
 
-
-static final kInitialPosition = LatLng(-31.4567844, -64.183108);
+  static final kInitialPosition = LatLng(-31.4567844, -64.183108);
 
   @override
   _MapsScreenState createState() => _MapsScreenState();
@@ -20,13 +18,11 @@ static final kInitialPosition = LatLng(-31.4567844, -64.183108);
 //CameraPosition _initialPosition = CameraPosition(target: LatLng(-31.8206, -64.8025));
 //Completer<GoogleMapController> _controller = Completer();
 
-
 //void _onMapCreated(GoogleMapController controller) {
 //    _controller.complete(controller);
 //}
 
 class _MapsScreenState extends State<MapsScreen> {
-
   PickResult selectedPlace;
 
   final List<String> listItemTitles = [
@@ -44,10 +40,10 @@ class _MapsScreenState extends State<MapsScreen> {
         Provider.of<DireccionRetiroProvider>(context).getDireccion;
 
     return Scaffold(
-      appBar: AppBar(
-          title: Text("Seleccioná un punto en el mapa"),
-          backgroundColor: Colors.redAccent),
-      body: Center(
+        appBar: AppBar(
+            title: Text("Seleccioná un punto en el mapa"),
+            backgroundColor: Colors.redAccent),
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -62,8 +58,8 @@ class _MapsScreenState extends State<MapsScreen> {
                         return PlacePicker(
                           apiKey: "AIzaSyCv8gS5tnyK0W2jQB1C3bbBC5Fa6NTKIik",
                           initialPosition: MapsScreen.kInitialPosition,
-                          useCurrentLocation: true,
-                          selectInitialPosition: true,
+                          useCurrentLocation: false,
+                          selectInitialPosition: false,
 
                           //usePlaceDetailSearch: true,
                           onPlacePicked: (result) {
@@ -76,31 +72,37 @@ class _MapsScreenState extends State<MapsScreen> {
                           //autocompleteLanguage: "ko",
                           //region: 'au',
                           //selectInitialPosition: true,
-                          selectedPlaceWidgetBuilder: (_, selectedPlace, state, isSearchBarFocused) {
-                            print("state: $state, isSearchBarFocused: $isSearchBarFocused");
+/*                           selectedPlaceWidgetBuilder:
+                              (_, selectedPlace, state, isSearchBarFocused) {
+                            print(
+                                "state: $state, isSearchBarFocused: $isSearchBarFocused");
                             return isSearchBarFocused
                                 ? Container()
                                 : FloatingCard(
-                                    bottomPosition: 0.0, // MediaQuery.of(context) will cause rebuild. See MediaQuery document for the information.
+                                    bottomPosition:
+                                        0.0, // MediaQuery.of(context) will cause rebuild. See MediaQuery document for the information.
                                     leftPosition: 0.0,
                                     rightPosition: 0.0,
                                     width: 500,
                                     borderRadius: BorderRadius.circular(12.0),
                                     child: state == SearchingState.Searching
-                                        ? Center(child: CircularProgressIndicator())
+                                        ? Center(
+                                            child: CircularProgressIndicator())
                                         : RaisedButton(
-                                            child: const Text("Pick Here"),
+                                            child: const Text("Buscar Aquí"),
                                             onPressed: () {
-
                                               
+                                              String text_humano = selectedPlace
+                                                  .formattedAddress;
                                               // IMPORTANT: You MUST manage selectedPlace data yourself as using this build will not invoke onPlacePicker as
                                               //            this will override default 'Select here' Button.
-                                              print("do something with [selectedPlace] data");
+                                              print(
+                                                  "do something with [$text_humano] data");
                                               Navigator.of(context).pop();
                                             },
                                           ),
                                   );
-                          },
+                          }, */
                           // pinBuilder: (context, state) {
                           //   if (state == PinState.Idle) {
                           //     return Icon(Icons.favorite_border);
@@ -114,15 +116,20 @@ class _MapsScreenState extends State<MapsScreen> {
                   );
                 },
               ),
-              selectedPlace == null ? Container() : Text(selectedPlace.formattedAddress ?? ""),
+              selectedPlace == null
+                  ? Container()
+                  : Text(selectedPlace.formattedAddress ?? ""),//----------- ACA ----------
+              TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: selectedPlace.formattedAddress),
+                    //"selectedPlace.formattedAddress" TIENE TEXTO CON DIRECCION human-readable
+              )
             ],
           ),
-        )
-
-    );
+        ));
   }
 }
-
 
 final Divider _divider = Divider(
   color: Colors.grey,
